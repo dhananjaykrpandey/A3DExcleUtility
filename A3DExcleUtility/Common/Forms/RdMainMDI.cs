971c51beed0e6ef2d85187d782bcd083bc313777
Dhananjay Kumar Pandey <dhananjaykrpandey@hotmail.com>
2020-09-29 17:00:01 +0400

    Add project files.

---
+++ A3DExcleUtility/Common/Forms/RdMainMDI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace A3DExcleUtility.Common.Forms
+{
+    public partial class RdMainMDI : Telerik.WinControls.UI.RadRibbonForm
+    {
+        public RdMainMDI()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ A3DExcleUtility/Program.cs
@@ -0,0 +1,43 @@
+﻿using A3DExcleUtility.Common.Forms;
+using A3DWinUtility;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A3DExcleUtility
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new RadForm1());
+            try
+            {
+                if (ClsUtility._IClsUtility.CheckLicense() == false)
+                {
+                    A3DLicense.FrmLicense ObjLic = new A3DLicense.FrmLicense();
+                    ObjLic.StartPosition = FormStartPosition.CenterScreen;
+                    ObjLic.ShowDialog();
+                }
+                else
+                {
+                    //InfSQLServices._IInfSQLServices.InfSQLConnectionString = GClsProjectProperties._IGClsProjectProperties.CProjectSqlConnection;
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new RdMainMDI());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
